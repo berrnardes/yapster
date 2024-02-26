2b3c9693a922310c1cd2904cefa0dbed44ca2274
bernardes <bernardes@BOOK-M3BRE6VD03>
2024-02-26 19:00:26 -0300

    Finishing landing page and starting the navigation

---
+++ src/components/navigation.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import MaxWidthWrapper from "./max-width-wrapper";
+
+const Navigation = () => {
+	return (
+		<div className="min-h-14 sticky inset-x-2 w-full bg-white/75 border-zinc-200 backdrop-blur-lg transition-all border-b top-0 z-30">
+			<MaxWidthWrapper>
+				<div className="flex min-h-14 justify-between items-center border-b border-zinc-200">
+					<h1 className="text-xl font-semibold text-zinc-700">
+						<Link href="/">Yapster</Link>
+					</h1>
+					{/* TODO MOBILE NAV */}
+
+					{/* CLERK IMPLEMENTATION */}
+					<div></div>
+				</div>
+			</MaxWidthWrapper>
+		</div>
+	);
+};
+
+export default Navigation;
